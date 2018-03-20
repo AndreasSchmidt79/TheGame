2176668ea3ce320b386162fbf4ec322a086dd0c9
JOHANNES-PC\Johannes <johannes.schmidt@it-bfw.de>
2018-03-20 21:32:45 +0100

    Random Map 1. Versuch

--- src/GameMap/MapGenerator.java
+++ src/GameMap/MapGenerator.java
@@ -1,5 +1,7 @@
 package GameMap;
 
+import java.util.Random;
+
 public class MapGenerator {
 	
 	public MapGenerator() {
@@ -20,8 +22,56 @@ public class MapGenerator {
 		return map;
 	}
 	private int[][] generateMap(){
-		int[][] gameMap = {
-				{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+		int dimensionsx = 19;
+		int dimensionsy = 19;
+		Random rand = new Random();
+		int[][] gameMap = new int[dimensionsx][dimensionsy];
+		for(int i = 0; i<dimensionsy; i++) {
+			for(int j=0;j<dimensionsx;j++) {
+				if(i==0 || i==dimensionsx || j==0 || j==dimensionsx) {
+					gameMap[i][j]=0;
+				}
+				else if (i-1==1 && j-1==1 || i-1==2 && j-1 == 2) {
+					int x=rand.nextInt(10);
+					if (x>=7) {
+						gameMap[i][j]=gameMap[i-1][j];
+					}
+					else {
+						x=rand.nextInt(10);
+						if(x>3){
+							gameMap[i][j]=1;
+						}	
+						else {
+							gameMap[i][j]=rand.nextInt(2)+2;
+						}
+					}					
+				}
+				else if (i-1==1 || i-1==2)  {
+					int x=rand.nextInt(10);
+					if (x>=4) {
+						gameMap[i][j]=gameMap[i-1][j];
+					}
+					else {
+						gameMap[i][j]=rand.nextInt(3);
+					}					
+				}
+				else if (j-1==1 || j-1==2)  {
+					int x=rand.nextInt(10);
+					if (x>=4) {
+						gameMap[i][j]=gameMap[i][j-1];
+					}
+					else {
+						gameMap[i][j]=rand.nextInt(3);
+					}
+				}
+				else {
+					gameMap[i][j]=rand.nextInt(3);
+				}
+			}
+		}
+				/* x = rand.nextInt(3);*/
+		
+				/*{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
 				{0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0},
 				{0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0},
 				{0,1,1,1,1,1,1,1,1,1,2,2,2,2,1,1,1,1,0},
@@ -40,7 +90,7 @@ public class MapGenerator {
 				{0,1,1,1,1,1,1,1,1,1,1,2,2,2,2,1,1,1,0},
 				{0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0},
 				{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
-			};
+			};*/
 		return gameMap;
 	}
 }
